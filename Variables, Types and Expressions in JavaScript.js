ad025bd5bfa9c45e111215eb298f50839f074cfa
Matys87 <73191950+Matys87@users.noreply.github.com>
2020-10-25 13:10:49 +0100

    Create Variables, Types and Expressions in JavaScript.js

---
+++ Variables, Types and Expressions in JavaScript.js
@@ -0,0 +1,20 @@
+let age = 2020 - 1987
+
+let ww2 = 1945 - 1939
+
+let teslaShare = 420.63
+let tesla = teslaShare * 76
+
+let secondsInHour = 60 * 60
+let secondsOfDay = 24 * secondsInHour
+
+let worldPopulation = 7800.0
+let hungarianPopulation = 9.8
+let hungarians = (hungarianPopulation / worldPopulation) * 100
+
+let sizeCzechRep = 78866
+let sizeChina = 9597000
+let bigger = sizeChina / sizeCzechRep
+
+let participants = 654 % 7
+
